fcc018dc7c488b5e029bd764fdc5132646ab50f3
Ken Doud <kmdoud@gmail.com>
2021-11-30 13:32:55 -0500

    moved to single project

---
+++ Context/SpgDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Dsi.Spg.Ui.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsi.Spg.Ui.Data.Context
+{
+    public class SpgDbContext : DbContext
+    {
+        public SpgDbContext(DbContextOptions<SpgDbContext> options) : base(options) {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {}
+
+        public DbSet<Member>? Members { get; set; }
+        public DbSet<Platform>? Platforms { get; set; }
+    }
+}
---
+++ Models/Member.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsi.Spg.Ui.Data.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Tag { get; set; }
+        public string? Discord { get; set; }
+        public string? Email { get; set; }
+        public List<Platform>? Platforms { get; set; }
+    }
+}
